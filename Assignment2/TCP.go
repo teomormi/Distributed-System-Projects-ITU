1afb90e601d64cd9c4a78b532ac115c18aece3cd
teomormi <matteo.mormile@mail.polimi.it>
2023-09-20 10:30:13 +0200

    Send message with channel

--- Assignment2/TCP.go
+++ Assignment2/TCP.go
@@ -12,6 +12,32 @@ import (
 	"fmt"
 )
 
+type Message struct {
+	Type    int
+	Seq     int
+	Payload string
+}
+
 func main() {
-	fmt.Printf("Hello world!")
+
+	network := make(chan Message)
+	go client(network)
+	go server(network)
+	for {
+	}
+	//fmt.Printf("Hello world!")
+	// 2 go routine simulate server and client
+}
+
+func server(channel chan Message) {
+	var msg Message
+	msg.Seq = 1
+	msg.Payload = "Hello"
+	msg.Type = 1
+	channel <- msg
+}
+
+func client(channel chan Message) {
+	answer := <-channel
+	fmt.Printf("Type %d Seq %d Payload %s", answer.Type, answer.Seq, answer.Payload)
 }

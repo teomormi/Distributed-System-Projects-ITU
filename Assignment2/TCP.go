982f8c8d2d1f8b74550431e10253f156a9f9bf0a
teomormi <matteo.mormile@mail.polimi.it>
2023-09-22 19:47:40 +0200

    Modify client
    Added some code to send message, it doesn't work, some problems with sequence id and ack id

--- Assignment2/TCP.go
+++ Assignment2/TCP.go
@@ -9,8 +9,10 @@ Authors:
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"math/rand"
+	"os"
 	"time"
 )
 
@@ -38,6 +40,8 @@ type Message struct {
 	Ack     int
 	Payload string // message content
 }
+
+var list []string
 
 func main() {
 
@@ -77,24 +81,43 @@ func server(channel chan Message) {
 		case Listen:
 			{
 				select {
+				// wait for the ack or go back to closed status (waiting for syn)
 				case msg := <-channel:
 					if msg.Type == Ack && msg.Ack == seq+1 && msg.Seq == ack { // estabilished connection
 						status = Established
+						// save last seq and ack to check the following messages
+						seq = msg.Seq
+						ack = msg.Ack
 						fmt.Println("Server status: Established")
 					}
 					break
-				case <-time.After(2 * time.Second):
+					/*case <-time.After(2 * time.Second):
 					status = Closed
-					break
+					break*/
 				}
-				// wait for the ack or go back to closed status (waiting for syn)
 				break
 			}
 		case Established:
 			{
 				// waiting for incoming data packets
-				//after data recieved
 				msg := <-channel
+				if msg.Type == Data && msg.Ack == ack && msg.Seq == seq+1 { // check seq and ack number
+					fmt.Printf("Message payload: %s\n", msg.Payload)
+					// test some delay or losses (retransit timeout 5 second - line 221)
+					time.Sleep(time.Duration(rand.Intn(4)) * time.Millisecond)
+					// reply with ack
+					seq = msg.Ack
+					ack = msg.Seq + 1
+					fmt.Printf("Server: Send ack to client Seq=%d Ack=%d\n", seq, ack)
+					reply := Message{
+						Type: Ack,
+						Ack:  ack,
+						Seq:  seq,
+					}
+					// save last seq and ack to check the following messages
+					channel <- reply
+				}
+
 				if msg.Type == Fin {
 					ack = msg.Seq + 1
 					finack := Message{
@@ -140,6 +163,9 @@ func client(channel chan Message) {
 	status := Closed
 	var seq int
 	var ack int
+	last := Message{
+		Type: -1,
+	}
 	fmt.Println("Client Status: Closed")
 
 	for {
@@ -168,6 +194,8 @@ func client(channel chan Message) {
 					channel <- msg
 					status = Established
 					fmt.Println("Client Status: Established")
+					// go routine for input
+					go input()
 				} else {
 					status = Closed
 					fmt.Println("Client Status: Closed")
@@ -180,7 +208,62 @@ func client(channel chan Message) {
 			}
 			break
 		case Established:
-
+			select {
+			case msg := <-channel:
+				if msg.Type == Ack && msg.Ack == seq+1 && msg.Seq == ack {
+					fmt.Println("Client: Received Ack")
+					if len(list) != 0 {
+						ack = msg.Seq
+						seq = msg.Ack + 1
+						fmt.Printf("Client: Send message to server Seq=%d Ack=%d\n", seq, ack)
+						next := Message{
+							Type:    Data,
+							Ack:     ack,
+							Seq:     seq,
+							Payload: list[0],
+						}
+						list = list[1:]
+						last = next
+						channel <- next
+					} else { // no more transimmision
+						last.Type = -1
+					}
+				}
+			case <-time.After(5 * time.Second): // resend or send the first packet
+				if last.Type == -1 { // no retransmittion
+					if len(list) > 0 { // at least one packet in the list
+						seq++
+						fmt.Printf("Client: Send message to server Seq=%d Ack=%d\n", seq, ack)
+						next := Message{
+							Type:    Data,
+							Ack:     ack,
+							Seq:     seq,
+							Payload: list[0],
+						}
+						list = list[1:] // remove first element from the list
+						last = next
+						channel <- next
+					}
+				} else {
+					// resend the packet
+					fmt.Println("Client: REsend message to server")
+					channel <- last
+				}
+			}
 		}
+	}
+}
+
+func input() {
+	var input string
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		input, _ = reader.ReadString('\n')
+
+		if input == "exit" {
+			break
+		}
+
+		list = append(list, input) // insert at end
 	}
 }
